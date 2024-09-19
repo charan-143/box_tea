087eacbdc706310bb95482ff2231fe3fa25e19ca
chara-143 <devanpallycharantej@klu.edu.in>
2024-09-19 19:32:57 +0530

    version 1.7

--- src/components/component/tea4.jsx
+++ src/components/component/tea4.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
 import {
 	Table,
 	TableHeader,
