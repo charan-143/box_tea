78b38a850c04e5895dfe47bc486958079ea3e9e5
chara-143 <devanpallycharantej@klu.edu.in>
2024-09-19 23:46:41 +0530

    version 2.4

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -56,7 +56,7 @@ export function Header({ setActiveTab, setIsProfileOpen, onLogout }) {
 		: userRole === 'worker' 
 		? [
 			{ href: "/worker-dashboard", label: "Worker Dashboard" },
-			,
+			
 		]
 		: [{ href: "/menu", label: "Menu" },
 			{ href: "/orders", label: "Orders" },];
